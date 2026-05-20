9ffb7b14cd18479883246ab1414b8abb91ca1249
Eesge Masekowsky <masekowsky.ee@gmail.com>
2026-05-19 23:32:24 -0400

    Add null check for subArray before rendering subjects

--- resources/scripts/combinedScript.js
+++ resources/scripts/combinedScript.js
@@ -12,10 +12,11 @@ console.log(eventListeners);
 
 const clockInterval = setInterval(renderObject.renderClock, 1000);
 
-subArrObj.subArray = JSON.parse(localStorage.getItem('subArray')) ?? [];
-console.log(subArrObj.subArray)
-subGenFunction(subArrObj.subArray, document.getElementById('subUl'), false);
-
+subArrObj.subArray = JSON.parse(localStorage.getItem('subArray')) ?? [ ];
+console.log(subArrObj.subArray);
+if(subArrObj.subArray[0]){
+    subGenFunction(subArrObj.subArray, document.getElementById('subUl'), false);
+}
 const inSevenDays = new Date(Date.now() + 7 * 24 * 3600 * 1000);
 document.getElementById('dateInput').value = inSevenDays.toISOString().split('T')[0];
 
